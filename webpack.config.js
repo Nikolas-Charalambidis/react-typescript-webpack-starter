58dfe4ba9e60ddcbfc9e6f7943c3eba730540494
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2020-12-06 17:56:49 +0100

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -9,25 +9,42 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer/lib/BundleAnalyzerPlugin");
 
-module.exports = (env) => {
-
-    const currentPath = path.join(__dirname);
-    const basePath = currentPath + '/.env';
-    const envPath = basePath + '.' + env.ENVIRONMENT;
-    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-    const fileEnv = DotEnv.config({ path: finalPath }).parsed;
-
-    // reduce it to a nice object, the same as before (but with the variables from the file)
-    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+// reduce it to a nice object, the same as before (but with the variables from the file)
+const reduceConfigMap = (source) => {
+    return Object.keys(source).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(source[next]);
         return prev;
     }, {});
+}
 
-    const isProduction = env.ENVIRONMENT === "production" || "production-analyze";
-    const isAnalyze = env.ENVIRONMENT === "production-analyze";
+module.exports = (env) => {
 
-    console.log(envKeys)
-    console.log(fileEnv);
+    console.log("Webpack configuration parameters:", env);
+    console.log("Picking the config-map .env file...");
+
+    const dirname = path.join(__dirname);
+    const baseEnvFilePath = dirname + '/.env';
+    const configuredEnvFilePath = baseEnvFilePath + '.' + env.ENVIRONMENT;
+    const envFilePath = fs.existsSync(configuredEnvFilePath) ? configuredEnvFilePath : baseEnvFilePath;
+    const envFile = DotEnv.config({ path: envFilePath }).parsed;
+    console.log("Picked .env file:", envFilePath);
+
+    console.log("Reducing the config-map parameters from the .env file...");
+    const envFileObject = reduceConfigMap(envFile);
+
+    console.log("Reducing the Webpack configuration parameters...")
+    const envConfigurationObject = reduceConfigMap(env);
+
+    console.log("Merging to the global environment parameters...")
+    const configMap = {...envFileObject, ...envConfigurationObject};
+
+    console.log("Global environment parameters:");
+    Object.entries(configMap).forEach(([key, value]) => console.log(" " + key + ": " + value));
+
+    const isProduction = env.ENVIRONMENT === "production";
+    const isAnalyze = env.ANALYZE;
+
+    console.log("Running Webpack...");
 
     return {
         context: __dirname,
@@ -85,7 +102,7 @@ module.exports = (env) => {
             isAnalyze ? new BundleAnalyzerPlugin() : false,
             new HtmlWebpackPlugin({template: "index.html"}),
             new MiniCssExtractPlugin(),
-            new webpack.DefinePlugin(envKeys),
+            new webpack.DefinePlugin(configMap),
             new ForkTsCheckerWebpackPlugin({
                 // For the dev server overlay to work
                 async: false
